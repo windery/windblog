3f3212f3ff829e20982d89a703a3c352a2834262
windery <res.windery@gmail.com>
2016-03-07 15:01:15 +0800

    add manager

--- helloworld.py
+++ helloworld.py
@@ -2,12 +2,18 @@
 #-*- coding: utf-8 -*-
 
 from flask import Flask
+from flask.ext.script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
-def index():
-    return 'hello world!'
+@app.route('/index/<name>')
+def index(name=None):
+    if name is None:
+        return '<h1>hello world!</h1>'
+    else:
+        return '<h1>hello %s!</h1>' % name
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
