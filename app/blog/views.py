67c3bad1322f0a0e23558aede085e41b6b676c8e
windery <res.windery@gmail.com>
2016-12-08 02:08:27 +0800

    add edit route

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, url_for, current_app, flash, redirect
 from . import blog
+from .forms import PostForm
 from .. import db
 
 def render_blog_template(template_name, **kwargs):
@@ -37,4 +38,18 @@ def thoughts():
 @blog.route('/about', methods=['GET'])
 def about():
     return render_blog_template('about.html')
+
+@blog.route('/edit', methods=['GET', 'POST'])
+def edit():
+    post_form = PostForm()
+    if post_form.validate_on_submit():
+        title = post_form.title.data
+        subject = post_form.subject.data
+        content = post_form.content.data
+        tags = post_form.tags.data
+        print('title:', title)
+        print('subject:', subject)
+        print('content:', content)
+        print('tags:', tags)
+    return render_blog_template('edit.html', post_form=post_form)
 
--- app/models.py
+++ app/models.py
@@ -1,8 +1,25 @@
 from . import db
 
-class Passage(db.Model):
-    __tablename__ = 'passage'
-    id = db.Column(db.BigInteger(unsigned=True), primary_key=True)
-    text = db.Column(db.Text(convert_unicode=False))
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.BigInteger(unsigned=True), primary_key=True, autoincrement=True)
+    subject = db.Column(db.String, db.ForeignKey('subject.name'))
+    title = db.Column(db.String)
+    content = db.Column(db.Text(convert_unicode=False))
     create_time = db.DateTime(db.DateTime())
     modify_time = db.DateTime(db.DateTime())
+    tags = db.Column(db.String)
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.BigInteger(unsigned=True), primary_key=True, autoincrement=True)
+    post_id = db.Column(db.BigInteger(unsigned=True), db.ForeighKey('post.id'))
+    parent_id = db.Column(db.BigInteger(unsigned=True), db.ForeignKey('comment.id'))
+    content = db.Column(db.String)
+    email = db.Column(db.String)
+    time = db.Column(db.DateTime)
+
+class Subject(db.Model):
+    __tablename__ = 'subject'
+    name = db.Column(db.String, primary_key=True)
