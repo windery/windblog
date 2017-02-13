468e9f87136b63748239769cbc5abfbaace93154
windery <res.windery@gmail.com>
2017-02-14 00:50:25 +0800

    open debug option in start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 
-python3 manage.py runserver --host 0.0.0.0 -p 80 -D >>blog.log 2>&1
+#python3 manage.py runserver --host 0.0.0.0 -p 80 -D >>blog.log 2>&1
+python3 manage.py runserver --host 0.0.0.0 -p 80 >>blog.log 2>&1
