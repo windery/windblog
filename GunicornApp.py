14540e386d0e77e2bf34ce4c1ebc09f46534276f
huanglang <res.windery@gmail.com>
2017-08-01 01:16:32 +0800

    add gunicorn support for performance

---
+++ GunicornApp.py
@@ -0,0 +1,46 @@
+from flask_script import Command, Option
+
+
+class GunicornApp(Command):
+
+    description = 'Run the app within Gunicorn'
+
+    def __init__(self, host='127.0.0.1', port=8000, workers=4):
+        self.port = port
+        self.host = host
+        self.workers = workers
+
+    def get_options(self):
+        return (
+            Option('-H', '--host',
+                   dest='host',
+                   default=self.host),
+
+            Option('-p', '--port',
+                   dest='port',
+                   type=int,
+                   default=self.port),
+
+            Option('-w', '--workers',
+                   dest='workers',
+                   type=int,
+                   default=self.workers),
+        )
+
+    def __call__(self, app=None, *args, **kwargs):
+        host = kwargs['host']
+        port = int(kwargs['port'])
+        workers = int(kwargs['workers'])
+
+        from gunicorn.app.base import Application
+        class FlaskApplication(Application):
+            def init(self, parser, opts, args):
+                return {
+                    'bind': '{0}:{1}'.format(host, port),
+                    'workers': workers
+                }
+
+            def load(self):
+                return app
+
+        FlaskApplication().run()
--- manage.py
+++ manage.py
@@ -4,14 +4,18 @@
 import os
 
 from flask_migrate import MigrateCommand
-from flask_script import Manager, Server
+from flask_script import Manager
+
+from GunicornApp import GunicornApp
 from app import create_app
 
 app = create_app(os.getenv('BLOG_CONFIG') or 'default')
 
 manager = Manager(app)
+
 manager.add_command('db', MigrateCommand)
-manager.add_command('runserver', Server(use_debugger=True, use_reloader=False))
+manager.add_command('runserver', GunicornApp())
+# manager.add_command('runserver', Server(use_debugger=True, use_reloader=False))
 
 if __name__ == '__main__':
    manager.run()
